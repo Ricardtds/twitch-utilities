e545b9eb5a57977fe71ddbfd01cb3f2591dcb36c
Ricardo Teixeira <ricardoteixeirasantos@hotmail.com>
2021-12-02 15:26:25 -0300

    Added last updated

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 
+function timeConverter(UNIX_timestamp){
+    let a = new Date(UNIX_timestamp * 1000);
+    return a.toLocaleString('en-GB', { timeZone: 'America/Sao_Paulo' });
+}
+
 app.get('/',(req,res)=>{
     res.sendStatus(200)
 })
 
-app.get('/moeda/',(req,res)=>{  
+app.get('/moeda/',(req,res)=>{
     const options = {
         method: 'GET',
         url: 'https://api.coingecko.com/api/v3/simple/price',
@@ -19,9 +24,11 @@ app.get('/moeda/',(req,res)=>{
     axios.request(options)
     .then(response=>{
         if(response.data[req.query.coin][req.query.vs_currencies] == undefined) {
-            res.send('Erro no nome da moeda')
-        } else {
-            res.send(`${response.data[`${req.query.coin}`][`${req.query.vs_currencies}`]}`)
+            res.send('Erro na moeda que deseja converter!')
+        }        
+        else {
+            let horario = timeConverter(response.data[req.query.coin]['last_updated_at'])
+            res.send(`${response.data[`${req.query.coin}`][`${req.query.vs_currencies}`]} dados de: ${horario}`)            
         }    
     }).catch((error)=>{
         res.send(`Erro ao obter moeda`)
