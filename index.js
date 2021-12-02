0fe0ec626995aada41de9435c07eca4b16b7eb64
Ricardo Teixeira <ricardoteixeirasantos@hotmail.com>
2021-12-02 14:49:56 -0300

    Começando a adicionar o tempo da última att

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express')
-const app = express()
 const axios = require('axios')
+const app = express()
 
 app.get('/',(req,res)=>{
     res.sendStatus(200)
 })
 
 app.get('/moeda/',(req,res)=>{  
-    axios.request(`https://api.coingecko.com/api/v3/simple/price?ids=${req.query.coin}&vs_currencies=${req.query.vs_currencies}`)
+    const options = {
+        method: 'GET',
+        url: 'https://api.coingecko.com/api/v3/simple/price',
+        params: {
+            vs_currencies: `${req.query.vs_currencies}`,
+            include_last_updated_at: 'true',
+            ids: `${req.query.coin}`
+        }
+    }
+    axios.request(options)
     .then(response=>{
         if(response.data[req.query.coin][req.query.vs_currencies] == undefined) {
             res.send('Erro no nome da moeda')
@@ -26,4 +35,3 @@ app.all('/*', (req,res)=>{
 app.listen(process.env.PORT || 3000, () =>{
     console.log('Servidor rodando!');
 })
-    
